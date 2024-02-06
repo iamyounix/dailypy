b71a58d93d9da889dbb2aaa92566a162633bc27b
⌘ younix <72515939+iamyounix@users.noreply.github.com>
2024-02-07 00:04:50 +0800

    Add files via upload

---
+++ dl.py
@@ -0,0 +1,33 @@
+import os
+import sys
+import requests
+from rich.progress import Progress
+
+def download_file(url, filename=None):
+    filename = filename or os.path.basename(url)
+    
+    try:
+        with requests.get(url, stream=True) as response:
+            response.raise_for_status()
+            total_size_in_bytes = int(response.headers.get('content-length', 0))
+            with Progress() as progress:
+                task_id = progress.add_task("download", filename=filename, total=total_size_in_bytes)
+                with open(filename, 'wb') as file:
+                    for chunk in response.iter_content(chunk_size=8192):
+                        file.write(chunk)
+                        progress.update(task_id, advance=len(chunk))
+                        
+    except requests.HTTPError as http_err:
+        print(f"HTTP error occurred: {http_err}")
+        sys.exit(1)
+    except Exception as err:
+        print(f"An error occurred: {err}")
+        sys.exit(1)
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Usage: python script.py URL [FILENAME]")
+        sys.exit(1)
+    url = sys.argv[1]
+    filename = sys.argv[2] if len(sys.argv) > 2 else None
+    download_file(url, filename)
